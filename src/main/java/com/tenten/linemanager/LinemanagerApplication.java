60ff834260f054b4f4948c4077fafb53223c7b68
taeyeonjang <wxd8339@gmail.com>
2026-07-01 18:38:09 +0900

    gitIgnore
    gitIgnore 추가

--- src/main/java/com/tenten/linemanager/LinemanagerApplication.java
+++ src/main/java/com/tenten/linemanager/LinemanagerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LinemanagerApplication {
 
 	public static void main(String[] args) {
+		//깃허브 수정 확인
 		SpringApplication.run(LinemanagerApplication.class, args);
 	}
 
