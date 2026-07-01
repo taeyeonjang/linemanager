ffc3e4e97fa45bcf2bf49a17d555ed3639efc40a
taeyeonjang <wxd8339@gmail.com>
2026-07-01 18:24:16 +0900

    springInit
    start project

---
+++ src/main/java/com/tenten/linemanager/LinemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.tenten.linemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenten/linemanager/LinemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenten.linemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
